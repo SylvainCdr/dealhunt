45afac22dd38308d0ac00c31cf083a9f4cd2c3d3
sly <sylvaincadoret@hotmail.com>
2026-03-19 17:14:32 +0100

    feat: cron Vercel + Google Search Console meta tag

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="71X_2vH96Y0O1sM5q7knyNigPPEsSSim7sxm7vMCQzU" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
