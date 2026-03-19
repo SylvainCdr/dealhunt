3c451299794f393d08cd0079a2f693c642e1d95d
sly <sylvaincadoret@hotmail.com>
2026-03-19 17:40:22 +0100

    feat: add Google Analytics (G-MDNM3TML2S)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,22 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="google-site-verification" content="71X_2vH96Y0O1sM5q7knyNigPPEsSSim7sxm7vMCQzU" />
+        <meta
+          name="google-site-verification"
+          content="71X_2vH96Y0O1sM5q7knyNigPPEsSSim7sxm7vMCQzU"
+        />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MDNM3TML2S"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MDNM3TML2S');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
